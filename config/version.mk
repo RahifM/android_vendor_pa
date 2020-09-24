e90419728f55d99527e5785bbaabe46f7719cde9
RahifM <faizel326@gmail.com>
2020-09-24 13:37:00 +0530

    update naming
    Signed-off-by: RahifM <faizel326@gmail.com>
    Change-Id: If2fbc3faa187ae6288393d44fdab6dcb609efb0d

--- config/version.mk
+++ config/version.mk
@@ -44,7 +44,7 @@ endif
 # BETA: Public builds for testing purposes
 # Public releases will not include a TAG
 ifndef PA_BUILDTYPE
-  PA_BUILD_VARIANT := SaveMe
+  PA_BUILD_VARIANT := GuardianAngel
 else
   ifeq ($(PA_BUILDTYPE), ALPHA)
     PA_BUILD_VARIANT := Alpha
